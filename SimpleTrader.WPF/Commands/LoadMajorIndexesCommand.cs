82d80fe8c280538a7fe70bef7b01b21981c4c133
Anecthor <christian.scholz@gmx.ch>
2021-08-09 11:02:29 +0200

    Loading implemented

---
+++ SimpleTrader.WPF/Commands/LoadMajorIndexesCommand.cs
@@ -0,0 +1,45 @@
+﻿using SimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleTrader.Domain.Services;
+using SimpleTrader.WPF.ViewModels;
+
+namespace SimpleTrader.WPF.Commands
+{
+    public class LoadMajorIndexesCommand : AsyncCommandBase
+    {
+        private readonly MajorIndexListingViewModel _viewModel;
+        private readonly IMajorIndexService _majorIndexService;
+
+
+        public LoadMajorIndexesCommand(MajorIndexListingViewModel viewModel, IMajorIndexService majorIndexService)
+        {
+            _viewModel = viewModel;
+            _majorIndexService = majorIndexService;
+        }
+        public override  async Task ExecuteAsync(object parameter)
+        {
+             await Task.WhenAll(LoadDowJones(), LoadNasDaq(), LoadSp500());
+
+             _viewModel.IsLoading = false;
+        }
+
+        private async Task LoadDowJones()
+        {
+            _viewModel.DowJones = await _majorIndexService.GetMajorIndex(MajorIndexType.DowJones);
+        }
+
+        private async Task LoadNasDaq()
+        {
+            _viewModel.Nasdaq = await _majorIndexService.GetMajorIndex(MajorIndexType.Nasdaq);
+        }
+
+        private async Task LoadSp500()
+        {
+            _viewModel.SP500 = await _majorIndexService.GetMajorIndex(MajorIndexType.SP500);
+        }
+    }
+}
